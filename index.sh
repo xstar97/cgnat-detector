0072c4e74e9b517d6b44563e0cf47e9f4ba9a6eb
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-09-08 18:23:09 -0400

    Update index.sh

--- index.sh
+++ index.sh
@@ -9,7 +9,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
     # This is a Windows system using MinGW or Cygwin
     echo "Running on Windows"
-	curl -s -o cgnat.bat -LJO https://github.com/xstar97/cgnat-detector/raw/main/scripts/win/cgnat.bat && chmod +x cgnat.bat && ./cgnat.bat
+	curl -s -O https://raw.githubusercontent.com/xstar97/cgnat-detector/main/scripts/win/cgnat.bat
+	cmd.exe /c "cgnat.bat"
 else
     echo "Unsupported operating system"
 fi
